b2cac70878b24b38dbbd9c441ae1ed2380ae8d0a
skgx <saurabhgupta2131@gmail.com>
2022-03-14 20:24:06 +0530

    destructure

---
+++ ES6/4_destructure.js
@@ -0,0 +1,15 @@
+let list=["audi","mercedes","Dodge"]
+let [car1,car2]=list
+let [car3,,car4]=list
+console.log(car1)
+console.log(car2)
+console.log(car3)
+console.log(car4)
+
+
+const person={
+    name:"Saurabh",
+    age:20
+}
+let {name,age}=person
+console.log(age)
