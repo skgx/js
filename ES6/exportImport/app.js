8a5cdff1c8d26dc763d312e9bcfebd0470536206
skgx <saurabhgupta2131@gmail.com>
2022-03-14 19:51:40 +0530

    modules

---
+++ ES6/exportImport/app.js
@@ -0,0 +1,20 @@
+//importing and exporting mdules
+
+import { hello } from './sell.js';
+import {data as da} from './sell.js' //importing data variable as name da 
+hello()
+console.log(da)
+
+
+//to import all the properties at once
+import * as bundle from './sell.js'
+bundle.hello()
+console.log(bundle.data)    
+
+//by deafult person object is imported
+import person from './customer.js'
+console.log(person)
+
+//by deafult person object is imported by name ps
+import ps from './customer.js'
+console.log(ps)
---
+++ ES6/exportImport/customer.js
@@ -0,0 +1,7 @@
+const person={
+    name:"saurabh"
+}
+
+//export by default
+//in 1 file only 1 object can be exported in this manner
+export default person;
