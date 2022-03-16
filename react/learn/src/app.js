8d8e19f0ef0163f41450089e933a72269dee7581
skgx <saurabhgupta2131@gmail.com>
2022-03-16 23:08:19 +0530

    sub component

--- react/learn/src/app.js
+++ react/learn/src/app.js
@@ -1,11 +1,18 @@
 import './app.css'
+import SubComponent from './components/SubComponent';
+
+// function App(){
+//     return (<div className="app-container">
+//                 <h3>First component</h3>
+//                 <p>This is a p tag</p>
+//             </div>
+//     )
+// }
 
 function App(){
-    return (<div className="app-container">
-                <h3>First component</h3>
-                <p>This is a p tag</p>
-            </div>
-    )
+    return (
+            <SubComponent />
+           )
 }
 
 export default App;
---
+++ react/learn/src/components/SubComponent.js
@@ -0,0 +1,9 @@
+import "./SubComponent.css"
+
+function SubComponent(){
+    return (
+        <h1 className="red">This is a sub component</h1>
+    )
+}
+
+export default SubComponent
