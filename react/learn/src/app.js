71ec75960a7368cac5e8e8af20d671f088cd2ee9
skgx <saurabhgupta2131@gmail.com>
2022-03-16 22:56:08 +0530

    first component

--- react/learn/src/app.js
+++ react/learn/src/app.js
@@ -1,5 +1,7 @@
+import './app.css'
+
 function App(){
-    return (<div>
+    return (<div className="app-container">
                 <h3>First component</h3>
                 <p>This is a p tag</p>
             </div>
