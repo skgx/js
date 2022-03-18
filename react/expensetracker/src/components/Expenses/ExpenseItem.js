cf994b67fc48744fa1bf893d663e116e1ead78f7
skgx <saurabhgupta2131@gmail.com>
2022-03-18 23:19:49 +0530

    updated normal fun to arrow function

--- react/expensetracker/src/components/Expenses/ExpenseItem.js
+++ react/expensetracker/src/components/Expenses/ExpenseItem.js
@@ -2,15 +2,23 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
-import React from 'react'
+import React,{useState} from 'react'
 const ExpenseItem=(props)=>{
+
+    const [title,setTitle]= useState(props.title);
+    const clickHandler=()=>{
+        setTitle('New Title')
+    }
+
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <input type="text"/>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
