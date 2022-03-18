e37eaaee8ba280131b422d5275e080d5dab793f9
skgx <saurabhgupta2131@gmail.com>
2022-03-18 13:00:12 +0530

    multiple props

--- react/expensetracker/src/App.js
+++ react/expensetracker/src/App.js
@@ -1,5 +1,5 @@
 
-import ExpenseItem from './components/ExpenseItem' 
+import Expenses from './components/Expenses' 
 
 function App() {
   let expenses=[
@@ -35,26 +35,7 @@ function App() {
   return (
     <div>
       <h2>Lets's GO</h2>
-    <ExpenseItem 
-      date={expenses[0].date} 
-      title={expenses[0].title}
-      amount={expenses[0].amount} 
-    />
-    <ExpenseItem 
-     date={expenses[1].date} 
-     title={expenses[1].title}
-     amount={expenses[1].amount} 
-    />
-    <ExpenseItem 
-      date={expenses[2].date} 
-      title={expenses[2].title}
-      amount={expenses[2].amount} 
-    />
-    <ExpenseItem 
-      date={expenses[3].date} 
-      title={expenses[3].title}
-      amount={expenses[3].amount} 
-    />
+      <Expenses item={expenses}/>
     </div>
   );
 }
