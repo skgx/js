9e13b4df7fae37cdc499c83ca261c16e50b7d58f
skgx <saurabhgupta2131@gmail.com>
2022-03-19 11:28:50 +0530

    expense form

--- react/expensetracker/src/components/Expenses/ExpenseItem.js
+++ react/expensetracker/src/components/Expenses/ExpenseItem.js
@@ -5,20 +5,27 @@ import Card from '../UI/Card'
 import React,{useState} from 'react'
 const ExpenseItem=(props)=>{
 
-    const [title,setTitle]= useState(props.title);
-    const clickHandler=()=>{
-        setTitle('New Title')
-    }
+    // const [newTitle,setNewTitle]= useState("");
+    
+
+    // const [title,setTitle]= useState(props.title);
+    // const clickHandler=()=>{
+    //     setTitle(newTitle)
+    // }
+
+    // const changeHandler=(event)=>{
+    //     setNewTitle(event.target.value)
+    // }
 
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <input type="text"/>
-            <button onClick={clickHandler}>Change title</button>
+            {/* <input type="text" value={newTitle} onChange={changeHandler}/>
+            <button onClick={clickHandler}>Change title</button> */}
         </Card>
     );
 }
